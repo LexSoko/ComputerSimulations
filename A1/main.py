209ca4c2619521371d994efe1c9bbed735ca83ca
Aleksey Sokolov <alekseysokolov63@gmail.com>
2024-03-18 23:03:26 +0100

    bayes doesnt work, everything else ready

--- A1/main.py
+++ A1/main.py
@@ -80,13 +80,14 @@ def hist_height_distribution(L, N , bins_needed = (0,9), plot = False):
         plt.show()
     return hist_height/w_height, bins , height_selected_bars
 
-def reject_method(generator,N, **kwargs ):
+def reject_method(generator,N,pdf_name = "rejection_mode",bin_size= 2, save = False, **kwargs ):
     leng = len( [key for key in kwargs.keys() if key[0] == "i"])
     x_total = np.zeros(leng,dtype=type(np.array([])))
     c_array = np.zeros(leng)
     env_array = np.zeros(leng, dtype=type(lambda x: x))
     transforms_array = np.zeros(leng, dtype=type(lambda x: x))
-    interval_array = []
+    
+    
     count_1 = 0
     for key, item in kwargs.items():
         if key[0] == 'i':
@@ -101,29 +102,49 @@ def reject_method(generator,N, **kwargs ):
             transforms_array[int(key[-1])] = item
     random_enveloped = []
     env_analytical = []
-    for env, tr, x , c in zip(env_array,transforms_array,x_total, c_array):
-        x_random_1 = generate_hist(N,zero_one=True)[2]
-        u = generate_hist(N,zero_one=0)[2]
-        x_t = tr(x_random_1)
-        
-        for xt, r in zip(x_t,u):
-            if c*r*env(xt) < generator(xt):
-                random_enveloped.append(xt)
-        env_analytical.append(env(x))
+    
+    for env, tr, x , c in tqdm(zip(env_array,transforms_array,x_total, c_array),desc="piecing envs together"):
+        _,_,x_random_1 = generate_hist(N,zero_one=True)
+        _,_,u = generate_hist(N,zero_one=True)
+       
+        x_t_inv = tr(x_random_1)
+        accept = 0
+        for xt_inv, r in tqdm(zip(x_t_inv,u),"rejecting random variables"):
+            if c*r*env(xt_inv) < generator(xt_inv):
+                random_enveloped.append(xt_inv)
+                accept += 1
+
+        env_analytical.append(c*env(x))
+    print("acceptance rate: ",accept/(N*len(env_array)) )
     env_analytical = np.concatenate(env_analytical,axis=None)    
     x_total = np.concatenate(x_total,axis=None)
-    fig_b3, ax_b3 = plt.subplots(1,1,figsize=[18,18])
-    ax_b3.hist(random_enveloped,bins=100, density=True)
-    ax_b3.plot(x_total,env_analytical)
-    ax_b3.plot(x_total,generator(x_total))
-    #ax_b3.hist(x_1,bins=100, density=True,histtype="step")
-    fig_b3.savefig("./A1/graphics/rejection_method_exp_dist_b3_new.pdf")
-    return random_enveloped
-        
 
+    bin_number = int(np.abs(max(random_enveloped)- min(random_enveloped))/bin_size)
+    if save:
+        fig_b, ax_b = plt.subplots(1,1,figsize=[18,18])
+        ax_b.hist(random_enveloped,bins=bin_number, density=True)
+        ax_b.set_xlim(min(x_total),max(x_total))
+        ax_b.plot(x_total,env_analytical)
+        ax_b.plot(x_total,generator(x_total))
+
+        fig_b.savefig(f"./A1/graphics/{pdf_name}.pdf")
+    return random_enveloped
+def inverse_method(inverse_cdf_func, N):
+    func_random = inverse_cdf_func(generate_hist(N,zero_one=True)[2])       
+    return func_random
+
+def get_bin_number(randoms,bin_width):
+    N = int(np.abs(max(randoms)- min(randoms))/bin_width)
+    return N
+def get_bin_center(randoms, bin_number):
+    array_center = np.linspace(min(randoms), max(randoms), bin_number)
+    return array_center
+def get_bayesian_expect(Ni,N,nb):
+    return (Ni + 1)/(N + nb + 1)
+def get_bayesian_error(expect,N,nb):
+    return np.sqrt((expect*(1-expect))/(N+nb+2))
 def gaussian(x,mu,std):
     return np.exp((-1/2)*((x-mu)/std)**2)/np.sqrt(2*np.pi*std**2)
-
 def bernoulli_error(Ni, N):
     return np.sqrt(Ni*(1- Ni/N))/N
 def bernoulli_error2(pi,N):
@@ -138,6 +159,14 @@ def exp_dist(x,lam):
     return 2*(np.exp(lam*x))
 def inverse_exp(x,lam):
     return (1/lam) * np.log(1-x)
+
+exp_dist_right = lambda x ,lam: np.where( x< 0.5 ,0.5*lam*np.exp(lam*x),  lam * 0.5*np.exp(-lam*x))
+inverse_exp_right = lambda x, lam: np.where(x<0.5, np.log(2*x)/lam, -np.log(2*(1-x))/lam)
+#exp_dist_right = lambda x ,lam: 0.5*np.exp(lam*x) if x < 0.5 else 1 - 0.5*np.exp(-lam*x)
+#inverse_exp_right = lambda x, lam: np.log(1/(2*(1-x)) if x < 0.5 else 2*x)/lam
+#x1 = np.arange(-50,50,1e-2)
+#plt.plot(x1, exp_dist_right(x1,0.5))
+#plt.show()
 def main(a1=True,ani_a1 = False, b1 = False, c1 = False , a2 = False, a3= False, b3=False,a4 = True):
    
     if a1 :
@@ -257,7 +286,7 @@ def main(a1=True,ani_a1 = False, b1 = False, c1 = False , a2 = False, a3= False,
         #x = np.arange(0,50,1e-2)
         #plt.plot(x,exp_dist(x,-0.5))
         plt.show()
-        #fig_a3.savefig("./A1/graphics/rejection_method_a3.pdf")
+        fig_a3.savefig("./A1/graphics/rejection_method_rwat_a3.pdf")
     if b3:
         x1 = np.arange(-50,0,1e-2)
         x2 = np.arange(0,50,1e-2)
@@ -272,124 +301,77 @@ def main(a1=True,ani_a1 = False, b1 = False, c1 = False , a2 = False, a3= False,
         for r , xt in zip(u1,x_t):
             if xt < 0:
                 
-                if -c*r*exp_dist(xt,0.5) < prob_dist(xt):
+                if c*r*exp_dist(xt,0.5) < prob_dist(xt):
                     rejection_random_b3.append(xt)
             if xt >= 0:
                 
                 if c*r*exp_dist(xt,-0.5) < prob_dist(xt):
                     rejection_random_b3.append(xt)
         x_total = np.concatenate([x1,x2],axis=None)
-        env_dist = np.concatenate([np.exp(x1/2)/2,np.exp(-x2/2)/2])
+        env_dist = c*np.concatenate([np.exp(x1/2)/2,np.exp(-x2/2)/2])
+        #env_dist = c*exp_dist_right(x_total,0.5)
+
         fig_b3, ax_b3 = plt.subplots(1,1,figsize=[18,18])
         ax_b3.hist(rejection_random_b3,bins=100, density=True)
         ax_b3.plot(x_total,env_dist)
         ax_b3.plot(x_total,prob_dist(x_total))
         #ax_b3.hist(x_1,bins=100, density=True,histtype="step")
-        fig_b3.savefig("./A1/graphics/rejection_method_exp_dist_b32.pdf")
-        randi = reject_method(lambda x: prob_dist(x),5000, 
-                              env0= lambda x: exp_dist(x,0.5),
-                              trans0 = lambda x: inverse_exp(x,0.5),
-                              inter0 = [-50,0],
-                              c0 = 2,
-                              env1= lambda x: exp_dist(x,-0.5),
-                              trans1 = lambda x: inverse_exp(x,-0.5),
-                              inter1 = [0,50],
-                              c1 = 2,
-                              )
+        fig_b3.savefig("./A1/graphics/rejection_method_exp_dist_test.pdf")
+    
 
     if a4:
         g=1
-        def reject_metho2d(generator,N, **kwargs ):
-            leng = len( [key for key in kwargs.keys() if key[0] == "i"])
-            x_total = np.zeros(leng,dtype=type(np.array([])))
-            c_array = np.zeros(leng)
-            env_array = np.zeros(leng, dtype=type(lambda x: x))
-            transforms_array = np.zeros(leng, dtype=type(lambda x: x))
-            interval_array = []
-            count_1 = 0
-            for key, item in kwargs.items():
-                if key[0] == 'i':
-                    index = int(float(key[-1]))
-                    x_total[index] = np.arange(item[0],item[1],1e-2)
-                    count_1 += 1
-                if key[0] == 'c':
-                    c_array[int(key[-1])] = item
-                if key[0] == 'e':
-                    env_array[int(key[-1])] = item
-                if key[0] == "t":
-                    transforms_array[int(key[-1])] = item
+        cauchy_inverse = inverse_method(lambda x: inverse_cdf(x), 10000)
 
-            random_enveloped = []
-            env_analytical = []
-            for env, tr, x , c in zip(env_array,transforms_array,x_total, c_array):
+        cauchy_envelope = reject_method( lambda x: prob_dist(x), 10000, pdf_name= "test8",bin_size=1,
+                            env0= lambda x: cauchy(x),
+                            trans0 = lambda x: inverse_cdf(x),
+                            inter0 = [-50,50],
+                            c0 =  2/(1-np.exp(-2))*1.02,)
+        print("acceptance rate should be ", 1/(2/(1-np.exp(-2))*1.02))
+        
+        exp_envelope = reject_method(lambda x: prob_dist(x),10000, pdf_name= "test_cond", bin_size=0.5, save=True,
+                            env0= lambda x: exp_dist_right(x,0.5),
+                            trans0 = lambda x: inverse_exp_right(x,0.5),
+                            inter0 = [-50,50],
+                            c0 = 3,)
+        print("acceptance rate should be ", 1/3)
+        generators = [cauchy_inverse,cauchy_envelope,exp_envelope] 
+        distributions = [lambda x: cauchy(x), lambda x: prob_dist(x),lambda x: prob_dist(x)]
+        histograms = []
+        bin_width = 0.5
 
-                x_random = generate_hist(N,zero_one=True)[2]
-                u = generate_hist(N,zero_one=0)[2]
-                x_t = tr(x_random)
-                
-                for xt, r in zip(x_t,u):
-                    if c*r*env(xt) < generator(xt):
-                        random_enveloped.append(xt)
-                env_analytical.append(env(x))
-            env_analytical = np.concatenate(env_analytical,axis=None)    
-            x_total = np.concatenate(x_total,axis=None)
-            fig_b33, ax_b33 = plt.subplots(1,1,figsize=[18,18])
-            ax_b33.hist(random_enveloped,bins=100, density=True)
-            ax_b33.plot(x_total,env_analytical)
-            ax_b33.plot(x_total,generator(x_total))
-            #ax_b3.hist(x_1,bins=100, density=True,histtype="step")
-            fig_b33.savefig("./A1/graphics/rejection_method_exp_dist_b3_new.pdf")
-            return random_enveloped
-                #def reject_method(generator,N, **kwargs ):
-                #    leng = len( [key for key in kwargs.keys() if key[0] == "i"])
-                #    x_total = np.zeros(len( leng),dtype=type(np.array([])))
-                #    c_array = np.zeros(leng)
-                #    env_array = np.zeros(leng, dtype=type(lambda x: x))
-                #    transforms_array = np.zeros(leng, dtype=type(lambda x: x))
-                #    interval_array = []
-                #    count_1 = 0
-                #    for key, item in kwargs.items():
-                #        if key[0] == 'i':
-                #            index = int(float(key[-1]))
-                #            x_total[index] = np.arange(item[0],item[1],1e-2)
-                #            count_1 += 1
-                #        if key[0] == 'c':
-                #            c_array[int(key[-1])] = item
-                #        if key[0] == 'e':
-        #            env_array[int(key[-1])] = item
-        #        if key[0] == "t":
-        #            transforms_array[int(key[-1])] = item
-        #    for env, tr, x , c in zip(env_array,transforms_array,x_total, c_array):
-        #        
-        #        x_random_1 = generate_hist(N,zero_one=True)[2]
-        #        u = generate_hist(N,zero_one=0)[2]
-        #        x_t = tr(x_random_1)
-        #        random_enveloped = []
-        #        if c*u*env(x_t) < generator(x_t):
-        #            random_enveloped.append(x_t)
-        #        env_analytical = np.concatenate([env(x) for x in x_total])
-        #        x_total = np.concatenate(x_total,axis=None)
-        #        fig_b3, ax_b3 = plt.subplots(1,1,figsize=[18,18])
-        #        ax_b3.hist(random_enveloped,bins=100, density=True)
-        #        ax_b3.plot(x_total,env_analytical)
-        #        ax_b3.plot(x_total,generator(x_total))
-        #        #ax_b3.hist(x_1,bins=100, density=True,histtype="step")
-        #        fig_b3.savefig("./A1/graphics/rejection_method_exp_dist_b3_new.pdf")
-#
-        #        
-#
+        fig_a4, ax_a4 = plt.subplots(3,2, figsize=[18,18])
+        for i, (g,d) in tqdm(enumerate(zip(generators,distributions)),desc = "generators loop"):
+            x = np.arange(-20,20,1e-2)
+            N = get_bin_number(g,bin_width)
+            center = get_bin_center(g,N)
+            hist , bin = np.histogram(g, bins=N)
+            print(hist)
+            hist2 , bin2 = np.histogram(g, bins=N, density= True)
+            frequentist_err = []
+            baysian_errors = []
+            bayesian_expect = []
+            for h in tqdm(hist,desc="hist loop"):
+                frequentist_err.append(np.sqrt(((h/len(g))*(1-h/len(g)))/len(g)))
+                bay_exp = get_bayesian_expect(h,len(g),N)
+                bayesian_expect.append(bay_exp/bin_width)
+                baysian_errors.append(get_bayesian_error(bay_exp, len(g), N))
+            ax_a4[i,0].hist(g,bins=N, density= True, label=f"frequentist {i}")
+            ax_a4[i,0].plot(x,d(x))
+            ax_a4[i,0].errorbar(center,hist2,frequentist_err, [0]*len(hist), fmt= ' ', capsize=1)
+            ax_a4[i,0].set_xlim(-20,20)
+            ax_a4[i,0].legend()
+            ax_a4[i,1].hist(g,bins=N, density= True, label=f"bayesian {i}")
+            ax_a4[i,1].plot(x,d(x))
+            ax_a4[i,1].errorbar(center, bayesian_expect, baysian_errors, [0]*len(hist),fmt= ' ', capsize=1)
+            ax_a4[i,1].set_xlim(-20,20)
+            ax_a4[i,1].legend()
 
+        fig_a4.savefig("./A1/graphics/baysiana4_test.pdf")
+        fig_a4.clf()
 
-
-
-
-
-        #reject_method(1, i0=(2,2),e2=2,i1=(2,1))
-
-
-
-
-            
+        
             
        
        
@@ -402,4 +384,18 @@ def main(a1=True,ani_a1 = False, b1 = False, c1 = False , a2 = False, a3= False,
     return
 
 
-main(a1 = False, b1 = False, c1 = False, a2 = False, a3 = False, b3=True, a4 = True)
+main(a1 = False, b1 = False, c1 = False, a2 = False, a3 = False, b3=False, a4 = True)
+
+
+
+#exp_envelope = reject_method(lambda x: prob_dist(x),10000, pdf_name= "test_exp", bin_size=1,
+#                              env0= lambda x: exp_dist(x,0.5),
+#                              trans0 = lambda x: inverse_exp(x,0.5),
+#                              inter0 = [-50,0],
+#                              c0 = 1.2,
+#                              env1= lambda x: exp_dist(x,-0.5),
+#                              trans1 = lambda x: inverse_exp(x,-0.5),
+#                              inter1 = [0,50],
+#                              c1 = 1.2,
+#                              )
+#        print("shouldbe ", 1/(1.2))
